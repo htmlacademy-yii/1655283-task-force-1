7bcdcb4b933122ea103977be1f614cd4ca6355a0
valer4a <toprootmail@gmail.com>
2021-12-26 16:24:01 +0200

    2612-04-commit

--- frontend/web/test.php
+++ frontend/web/test.php
@@ -11,26 +11,6 @@ use Classes\Actions;
 $object = new Actions(10,10,20);
 
 
- try {
-
-        //echo "status new:".$object->getStatus('new')."<br>";
-        echo "action response:".$object->getAction('finish')."<br>";
-        //echo "new-response:".$object->getNextStatus('process','new')."<br>";
-        //echo "a-client:".$object->getActionsClient('new')."<br>";
-        }
-        catch(MyException $e) {
-            echo "MyExc:".$e->getMessage();
-        }
-         catch (\Exception | \Error $error) {
-            echo "<PRE>";
-            print_r([
-                'error_message' => $error->getMessage(),
-                'error_file' => $error->getFile(),
-                'error' => $error->getLine()
-            ]);
-            echo "</PRE>";
-        }
-
 
 
 
